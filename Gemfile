cfbeded1413be60ceba4468a562baeb6473eea38
Oliver Switzer <oliver@kickstarter.com>
2018-12-27 18:02:53 -0500

    Balloon bot now sets the status for #balloon-bot-test channel

--- Gemfile
+++ Gemfile
@@ -3,3 +3,7 @@ source 'https://rubygems.org'
 gem 'slack-ruby-bot'
 gem 'async-websocket'
 gem 'dotenv'
+
+group :development, :test do
+  gem 'pry'
+end
--- balloon-bot.rb
+++ balloon-bot.rb
@@ -1,8 +1,16 @@
 require 'slack-ruby-bot'
+require 'pry'
 
 class BalloonBot < SlackRubyBot::Bot
-  command 'hold' do |client, data, match|
-    client.say(channel: data.channel, text: 'holding')
+  command 'hold deploys' do |client, data, match|
+    client.say(channel: data.channel, text: 'Holding deploys!')
+    client.web_client.channels_setTopic(channel: "#balloon-bot-test", topic: '⚠️ Hold deploys ⚠️')
+  end
+
+  command 'green' do |client, data, match|
+    client.say(channel: data.channel, text: 'Nice. Setting #balloon-bot-test channel topic back to green.')
+    client.web_client.channels_setTopic(channel: "#balloon-bot-test", topic: ':green_balloon: :circleci-pass:')
+    client.say
   end
 end
 
