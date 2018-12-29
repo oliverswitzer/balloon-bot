c85d216826aa1dfdce20cf773c9acc92f6974baa
Oliver Switzer <oliverswitzer@gmail.com>
2018-12-29 09:08:04 +0100

    Able to hook into message events to begin gathering conversations during outtages. Need to figure out best/most efficient way to only listen to messages from #deployments
    * Should I look up the channel and make sure it matches #deployments or #incident-response?
    Or is there a better way that wont trigger this event all the time.

--- balloon-bot.rb
+++ balloon-bot.rb
@@ -2,6 +2,9 @@ require 'slack-ruby-bot'
 require 'pry'
 require 'dotenv'
 
+Dotenv.load!
+
+
 class BalloonBot < SlackRubyBot::Bot
   command 'hold deploys' do |client, data, match|
     client.say(channel: data.channel, text: 'Holding deploys!')
@@ -15,6 +18,20 @@ class BalloonBot < SlackRubyBot::Bot
   end
 end
 
-Dotenv.load!
+MESSAGES = []
 
+module Hooks
+  class Message
+    def call(client, data)
+      channel = client.web_client.channels_info(channel: data['channel'])['channel']
+      if channel['name'] == 'balloon-bot-test'
+        MESSAGES << data['text']
+        puts "Here are all the messages that have been sent: "
+        puts MESSAGES.join("\n")
+      end
+    end
+  end
+end
+
+BalloonBot.instance.on(:message, Hooks::Message.new)
 BalloonBot.run
