d8e9c1e4fc73842de21ee5289de8b07af3217e7f
Oliver Switzer <oliverswitzer@gmail.com>
2018-12-29 09:24:49 +0100

    Extract DEPLOYMENTS_CHANNEL constant so it will be easy to point this bot at #deployments later when its ready to go

--- balloon-bot.rb
+++ balloon-bot.rb
@@ -4,16 +4,17 @@ require 'dotenv'
 
 Dotenv.load!
 
+DEPLOYMENTS_CHANNEL = 'balloon-bot-test'.freeze
 
 class BalloonBot < SlackRubyBot::Bot
   command 'hold deploys' do |client, data, match|
     client.say(channel: data.channel, text: 'Holding deploys!')
-    client.web_client.channels_setTopic(channel: "#balloon-bot-test", topic: '⚠️ Hold deploys ⚠️')
+    client.web_client.channels_setTopic(channel: "##{DEPLOYMENTS_CHANNEL}", topic: '⚠️ Hold deploys ⚠️')
   end
 
   command 'green' do |client, data, match|
-    client.say(channel: data.channel, text: 'Nice. Setting #balloon-bot-test channel topic back to green.')
-    client.web_client.channels_setTopic(channel: "#balloon-bot-test", topic: ':green_balloon: :circleci-pass:')
+    client.say(channel: data.channel, text: "Nice. Setting ##{DEPLOYMENTS_CHANNEL} channel topic back to green.")
+    client.web_client.channels_setTopic(channel: "##{DEPLOYMENTS_CHANNEL}", topic: ':green_balloon: :circleci-pass:')
     client.say
   end
 end
@@ -24,7 +25,7 @@ module Hooks
   class Message
     def call(client, data)
       channel = client.web_client.channels_info(channel: data['channel'])['channel']
-      if channel['name'] == 'balloon-bot-test'
+      if channel['name'] == DEPLOYMENTS_CHANNEL
         MESSAGES << data['text']
         puts "Here are all the messages that have been sent: "
         puts MESSAGES.join("\n")
