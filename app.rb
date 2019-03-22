ef84ff040632d45f5e23948417d3b6a89c6c1bbb
Oliver Switzer <oliver@kickstarter.com>
2019-03-22 18:22:06 -0400

    Whoops, forgot to remove rufus-scheduler requires in app.rb

--- app.rb
+++ app.rb
@@ -1,24 +1,9 @@
-require 'rufus-scheduler'
-
-require './clients/slack/slack_client_wrapper'
 require './clients/slack/balloon-bot'
-require './persistence/persistence'
 
 class App
   def self.run
     BalloonBot.instance.on(:message, Hooks::Message.new)
     BalloonBot.run
-
-    Rufus::Scheduler.new.every '10s' do
-      # `send` is a bit of a hack here, but AFAIK this is the only
-      # way to get access to the slack bot client outside of
-      # the BalloonBot/Hooks class
-      UpdatePullRequestStatuses.new(
-        incidents_repository: Persistence::INCIDENTS_REPOSITORY,
-        messages_repository: Persistence::MESSAGES_REPOSITORY,
-        chat_client: SlackClientWrapper.new(slack_bot_client)
-      ).execute
-    end
   end
 
   def self.slack_bot_client
