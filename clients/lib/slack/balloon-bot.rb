8ee74c9a5c393eab3d9b4c71401d16114ac7ef8e
Oliver Switzer <oliver@kickstarter.com>
2019-03-26 18:49:32 -0400

    Add 'help' command that explains possible commands to run
    [Finishes #164842351]

--- clients/lib/slack/balloon-bot.rb
+++ clients/lib/slack/balloon-bot.rb
@@ -27,6 +27,21 @@ class BalloonBot < SlackRubyBot::Bot
       github_client: GithubClientWrapper.new
     ).execute
   end
+
+  help do
+    title 'BalloonBot'
+    desc 'This Slack bot is meant to help let engineers in your organization know when master is broken.'
+
+    command 'hold deploys' do
+      desc 'will change the channel topic to a failing icon and prevent merges into master via a failing github status check. It is best to provide a reason for the failure'
+      long_desc 'Usage: "@balloonbot hold deploys <optional description of the issue>"'
+    end
+
+    command 'green' do
+      desc 'will change the channel topic back to a passing icon and allow merges into master again'
+      long_desc('Usage: "@balloonbot green"')
+    end
+  end
 end
 
 module Hooks
