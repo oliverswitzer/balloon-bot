c5d90f5868addf816d8aadc78ce4bb8d812449e4
Oliver Switzer <oliver@kickstarter.com>
2019-05-17 19:18:56 -0400

    Expose API endpoint where people can query for the last n incidents and their assosciated messages
    NOTES:
    * ADD AUTHORIZATION BEFORE PUSHING UP TO STAGING OR PROD. This exposes
    incident data to the public
    * You added webpacker a few commits back. Figure out if you still wanna
    use it
    
    [#166097274]

--- web/app/controllers/incidents_controller.rb
+++ web/app/controllers/incidents_controller.rb
@@ -1,7 +1,13 @@
 class IncidentsController < ApplicationController
   def index
-    @incidents = Persistence::INCIDENTS_REPOSITORY.find_last_n_with_messages(10).to_json
+    @incidents = Persistence::INCIDENTS_REPOSITORY.find_last_n_with_messages(
+      safe_params[:last].to_i
+    ).to_json
 
     render json: @incidents
+  end
+
+  private def safe_params
+    params.permit(:last)
   end
 end
