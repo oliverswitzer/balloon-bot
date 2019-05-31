134dff1eebcfc4409b7e4b7a98acdb0115825248
Oliver Switzer <oliver@kickstarter.com>
2019-05-31 18:52:19 -0400

    Add web view for all incidents

--- web/app/controllers/incidents_controller.rb
+++ web/app/controllers/incidents_controller.rb
@@ -6,9 +6,12 @@ class IncidentsController < ApplicationController
   def index
     @incidents = Persistence::INCIDENTS_REPOSITORY.find_last_n_with_messages(
       safe_params[:last].to_i
-    ).to_json
+    )
 
-    render json: @incidents
+    respond_to do |format|
+      format.json { render json: @incidents }
+      format.html { render :index }
+    end
   end
 
   private def safe_params
