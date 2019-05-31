484c47088a21dde87c4f06c8d63689c6ea205401
Oliver Switzer <oliver@kickstarter.com>
2019-05-31 19:39:55 -0400

    move auth back down to incidents controller

--- web/app/controllers/application_controller.rb
+++ web/app/controllers/application_controller.rb
@@ -1,5 +1,2 @@
 class ApplicationController < ActionController::Base
-  if %w[staging production].include? Rails.env
-    http_basic_authenticate_with name: ENV['HTTP_AUTH_USER'], password: ENV['HTTP_AUTH_PASSWORD']
-  end
 end
