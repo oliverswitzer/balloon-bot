e232f707d959041cafe1c738c7209b3787dba263
Oliver Switzer <oliver@kickstarter.com>
2019-06-04 16:12:09 -0400

    Remove types and make client.rb only file in root dir of clients

--- clients/lib/clients.rb
+++ clients/lib/clients.rb
@@ -1,10 +1,8 @@
-module Clients
-  require 'persistence'
-  require 'core'
+require 'persistence'
+require 'core'
 
-  require 'slack-ruby-bot'
-  require 'types'
-  require 'github/github_client_wrapper'
-  require 'slack/slack_client_wrapper'
-  require 'slack/balloon-bot'
-end
+require 'slack-ruby-bot'
+require 'types/types'
+require 'github/github_client_wrapper'
+require 'slack/slack_client_wrapper'
+require 'slack/balloon-bot'
