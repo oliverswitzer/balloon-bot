f159f76b9bdabf3b94b56166dc1dcf2022b03dbd
Oliver Switzer <oliverswitzer@gmail.com>
2019-06-25 08:07:42 +0100

    Turns out we still need the ActiveRecord::Base.establish_connection in persistence spec_helper since Rails isn't around to do that magic for us

--- persistence/spec/spec_helper.rb
+++ persistence/spec/spec_helper.rb
@@ -3,6 +3,8 @@ require 'test_support'
 require 'persistence'
 require 'core'
 
+ActiveRecord::Base.establish_connection("postgresql://localhost:5432/balloon_bot_test")
+
 # Code below is supposed to run migrations before tests get run. However, I am getting
 # and error because I think we are installing the wrong version of ActiveRecord (one
 # where the method `up` no longer exists on ActiveRecord::Migrator)
@@ -10,7 +12,6 @@ require 'core'
 # Need to figure out how to lock in the ActiveRecord version specified in the Gemfile.lock
 # of this gem (Robert said that this was possible)
 #
-# ActiveRecord::Base.establish_connection("postgresql://localhost:5432/balloon_bot_test")
 # migrations_path = File.join(__dir__, "..", "lib", "migrations")
 # ActiveRecord::Migrator.up(migrations_path)
 
