ca7fe2635f35c3b11487e31bcdd5d25a17594790
Oliver Switzer <oliver@kickstarter.com>
2019-08-09 00:27:09 -0400

    Remove unecessary dependency on babel stuff. Lets just use typescript

--- web/javascripts/webpack.config.js
+++ web/javascripts/webpack.config.js
@@ -13,13 +13,6 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader'
-        }
-      },
-      {
         test: /\.tsx?$/,
         use: 'ts-loader',
         exclude: /node_modules/
