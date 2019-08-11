b84608e1d01483c84903881844d1ff24cde5cbee
Oliver Switzer <oliver@kickstarter.com>
2019-08-11 00:18:45 -0400

    Add primereact as dependency
    * Modify webpack loaders to handle css of primereact

--- web/javascripts/incidents/main.tsx
+++ web/javascripts/incidents/main.tsx
@@ -1,3 +1,7 @@
+import 'primereact/resources/themes/nova-dark/theme.css';
+import 'primereact/resources/primereact.min.css';
+import 'primeicons/primeicons.css';
+
 import * as React from 'react';
 import { useFetch } from '../hooks/use-fetch';
 import { Incident } from './types';
